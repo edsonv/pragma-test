e254764e66035597f1643781873527b95027c3dc
Edson J Vargas <edson.jhair@yahoo.com>
2022-03-16 01:32:38 -0500

    Updates

---
+++ precache-manifest.dae3fa8a919a8b47722ac5e473653867.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801499a25e4f4181332ac7542896c1cc",
+    "url": "/pragma-test/index.html"
+  },
+  {
+    "revision": "d96cab5237956cc6f85d",
+    "url": "/pragma-test/static/css/4.4932a258.chunk.css"
+  },
+  {
+    "revision": "1eb8bd4bcb1ca2bcf83f",
+    "url": "/pragma-test/static/css/main.faf8c058.chunk.css"
+  },
+  {
+    "revision": "d96cab5237956cc6f85d",
+    "url": "/pragma-test/static/js/4.2673072c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pragma-test/static/js/4.2673072c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "314ec4df0cf4e8480d4f",
+    "url": "/pragma-test/static/js/5.0fbcd2ad.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/pragma-test/static/js/5.0fbcd2ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63cf6fbef9edde276aff",
+    "url": "/pragma-test/static/js/6.b1473cc5.chunk.js"
+  },
+  {
+    "revision": "1eb8bd4bcb1ca2bcf83f",
+    "url": "/pragma-test/static/js/main.bfd1fdce.chunk.js"
+  },
+  {
+    "revision": "02a65bd93db8eba963a7",
+    "url": "/pragma-test/static/js/runtime-main.ba169bcb.js"
+  },
+  {
+    "revision": "2d48eb0131ccd504324e",
+    "url": "/pragma-test/static/js/stencil-h8k-logo_2-entry-js.9f141d0e.chunk.js"
+  },
+  {
+    "revision": "84ee2de73a63424a1d03",
+    "url": "/pragma-test/static/js/stencil-polyfills-css-shim.5bed6002.chunk.js"
+  }
+]);
